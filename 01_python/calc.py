e169440a122c5cbe175576ec8ac69d91936258ad
Harinne <chek.zip@gmail.com>
2026-04-15 17:54:54 +0900

    oop&모듈

---
+++ 01_python/calc.py
@@ -0,0 +1,19 @@
+__version__ = 0.1 #변수
+
+def plus(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+def mutiply(num1, num2):
+    return num1 * num2
+
+def divid(num1, num2):
+    return num1 / num2
+
+result = minus(10,5) #함수 호출 -> 기본 : 같은 모듈에 정의된 함수를 호출
+print(result)
+
+
+# python calc.py 
---
+++ 01_python/run.py
@@ -0,0 +1,7 @@
+#calc 모듈의 함수 호출을 위해 calc모듈 import 
+import calc
+
+result = calc.plus(20,30)
+print(result)
+
+#python run.py
